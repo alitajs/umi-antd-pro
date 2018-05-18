3f914d92d84cedec01f7400aa24d1c6a0bb784b4
xiaohuoni <448627663@qq.com>
2018-05-18 18:25:40 +0800

    去掉手动禁用mock的环境变量 因为umi1.3.3已经内置了MOCK=none

--- .umirc.mock.js
+++ .umirc.mock.js
@@ -1,21 +1,20 @@
-import antdServer from 'antd-pro-server';
-import mockjs from 'mockjs';
-import { getRule, postRule } from './mock/rule';
-// // 是否禁用代理
-const noProxy = process.env.NO_PROXY === 'true';
+import antdServer from "antd-pro-server";
+import mockjs from "mockjs";
+import { getRule, postRule } from "./mock/rule";
+
 //支持扩展
 //支持从mock文件夹引入
 //支持mockjs
 const proxy = {
-  'GET /api/test': [
+  "GET /api/test": [
     {
-      code:'success',
-      message:'成功'
-    },
+      code: "success",
+      message: "成功"
+    }
   ],
-  'GET /api/test/rule': getRule,
-  'GET /api/test/tags': mockjs.mock({
-    'list|100': [{ name: '@city', 'value|1-100': 150, 'type|0-2': 1 }],
-  }),
+  "GET /api/test/rule": getRule,
+  "GET /api/test/tags": mockjs.mock({
+    "list|100": [{ name: "@city", "value|1-100": 150, "type|0-2": 1 }]
+  })
 };
-export default (noProxy ? {} : {...proxy,...antdServer});
+export default { ...proxy, ...antdServer };
