74ad474b25797754801aa6aef01cb5ef5be0b8f9
小虎Oni <xiaohuoni@users.noreply.github.com>
2018-05-19 16:40:37 +0800

    Update AuthorizedRoute.js

--- src/components/Authorized/AuthorizedRoute.js
+++ src/components/Authorized/AuthorizedRoute.js
@@ -1,16 +1,33 @@
 import React from 'react';
 import { Route, Redirect } from 'react-router-dom';
 import Authorized from './Authorized';
+import { getRouterData } from 'common/router';
 
+//umi只会返回render和rest
 class AuthorizedRoute extends React.Component {
   render() {
-    const { component: Component, render, authority, redirectPath, ...rest } = this.props;
+    const {
+      render,
+      path,
+      ...rest
+    } = this.props;
+    const routerData = getRouterData({});
+    const redirectPath = '/User/Login';
+    const authority = routerData[path].authority;
     return (
       <Authorized
         authority={authority}
-        noMatch={<Route {...rest} render={() => <Redirect to={{ pathname: redirectPath }} />} />}
+        noMatch={
+          <Route
+            {...rest}
+            render={() => <Redirect to={{ pathname: redirectPath }} />}
+          />
+        }
       >
-        <Route {...rest} render={props => (Component ? <Component {...props} /> : render(props))} />
+        <Route
+          {...rest}
+          render={props =>render(props)}
+        />
       </Authorized>
     );
   }
