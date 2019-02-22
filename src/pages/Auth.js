a7f711c73d8565ae0ffbbe37128b498d1a2728b7
xiaohuoni <448627663@qq.com>
2019-02-22 23:48:12 +0800

    Authorized

--- src/pages/Auth.js
+++ src/pages/Auth.js
@@ -1,40 +1,47 @@
-import React from 'react';
+import React, { Component } from 'react';
+import Redirect from 'umi/redirect';
 import pathToRegexp from 'path-to-regexp';
+import { connect } from 'dva';
 import Authorized from '@/utils/Authorized';
-import Exception403 from './Exception/403';
 
-export default ({ children, location }) => {
-  /* eslint-disable no-underscore-dangle */
-  const { routerData } = window.g_app._store.getState().menu;
-  const getRouteAuthority = (pathname, routeData) => {
-    const routes = routeData.slice(); // clone
+class AuthComponent extends Component {
+  render() {
+    const { children, location, routerData, status } = this.props;
+    const isLogin = status === 'ok';
 
-    const getAuthority = (routeDatas, path) => {
-      let authorities;
-      routeDatas.forEach(route => {
-        // check partial route
-        if (pathToRegexp(`${route.path}(.*)`).test(path)) {
-          if (route.authority) {
-            authorities = route.authority;
+    const getRouteAuthority = (pathname, routeData) => {
+      const routes = routeData.slice(); // clone
+
+      const getAuthority = (routeDatas, path) => {
+        let authorities;
+        routeDatas.forEach(route => {
+          // check partial route
+          if (pathToRegexp(`${route.path}(.*)`).test(path)) {
+            if (route.authority) {
+              authorities = route.authority;
+            }
+            // is exact route?
+            if (!pathToRegexp(route.path).test(path) && route.routes) {
+              authorities = getAuthority(route.routes, path);
+            }
           }
-          // is exact route?
-          if (!pathToRegexp(route.path).test(path) && route.routes) {
-            authorities = getAuthority(route.routes, path);
-          }
-        }
-      });
-      return authorities;
+        });
+        return authorities;
+      };
+
+      return getAuthority(routes, pathname);
     };
-
-    return getAuthority(routes, pathname);
-  };
-
-  return (
-    <Authorized
-      authority={getRouteAuthority(location.pathname, routerData)}
-      noMatch={<Exception403 />}
-    >
-      {children}
-    </Authorized>
-  );
-};
+    return (
+      <Authorized
+        authority={getRouteAuthority(location.pathname, routerData)}
+        noMatch={isLogin ? <Redirect to="/exception/403" /> : <Redirect to="/user/login" />}
+      >
+        {children}
+      </Authorized>
+    );
+  }
+}
+export default connect(({ menu: menuModel, login: loginModel }) => ({
+  routerData: menuModel.routerData,
+  status: loginModel.status,
+}))(AuthComponent);
