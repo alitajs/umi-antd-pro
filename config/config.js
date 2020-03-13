a9755cf040d972507bd588d01c0f18729268aafe
xiaohuoni <448627663@qq.com>
2020-03-13 13:43:45 +0800

    chore: 修改 cssLoaderOptions 命名为 cssLoader

--- config/config.js
+++ config/config.js
@@ -45,6 +45,28 @@ export default {
   //     pathRewrite: { '^/server': '' },
   //   },
   // },
+  cssLoader: {
+    modules: true,
+    getLocalIdent: (context, localIdentName, localName) => {
+      if (
+        context.resourcePath.includes('node_modules') ||
+        context.resourcePath.includes('ant.design.pro.less') ||
+        context.resourcePath.includes('global.less')
+      ) {
+        return localName;
+      }
+      const match = context.resourcePath.match(/src(.*)/);
+      if (match && match[1]) {
+        const antdProPath = match[1].replace('.less', '');
+        const arr = slash(antdProPath)
+          .split('/')
+          .map(a => a.replace(/([A-Z])/g, '-$1'))
+          .map(a => a.toLowerCase());
+        return `antd-pro${arr.join('-')}-${localName}`.replace(/--/g, '-');
+      }
+      return localName;
+    },
+  },
   ignoreMomentLocale: true,
   chainWebpack: webpackPlugin,
 };
